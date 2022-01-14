5ae2627a655f11c9dbd8efa6ac7f018a8ac51fdf
man.wang <man.wang@symbio.com>
2022-01-14 22:20:29 +0800

    初始化，多页面

---
+++ blog/src/admin/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      admin page
+    </div>
+  );
+}
+
+export default App;
---
+++ blog/src/front/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      front Page
+    </div>
+  );
+}
+
+export default App;
